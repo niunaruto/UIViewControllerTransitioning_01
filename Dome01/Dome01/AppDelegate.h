43deb3168763c8d174637fb65adbbc9ae9f8d7b9
nt <niuting@imcoming.cn>
2016-05-26 11:27:40 +0800

    dome1

---
+++ Dome01/Dome01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dome01
+//
+//  Created by anlaiye on 16/5/26.
+//  Copyright © 2016年 wangmingmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dome01/Dome01Tests/Dome01Tests.m
@@ -0,0 +1,39 @@
+//
+//  Dome01Tests.m
+//  Dome01Tests
+//
+//  Created by anlaiye on 16/5/26.
+//  Copyright © 2016年 wangmingmin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Dome01Tests : XCTestCase
+
+@end
+
+@implementation Dome01Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
